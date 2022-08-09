e20a87f2f2ea0e83139564a3b1da4e0485e6e9c2
Copple <10214025+kiwicopple@users.noreply.github.com>
2022-08-09 14:34:31 +0200

    REmoves next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
   },
